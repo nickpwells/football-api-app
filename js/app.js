9a7101c9bf9da31ecec09be274e5d09e0c46b23c
nickpwells <wells.nick53@gmail.com>
2015-06-22 10:05:42 -0700

    index.html

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	$.ajax({
+  		type: 'POST',
+  		url: 'https://profootballapi.com/players',
+  		data: {"api_key": "RVmnd4sEiJY7PeA0MvQ5UquZW1poBGa3",
+  				"stats_type": "offense"},
+  		dataType: 'jsonp',
+	});
+});
