502eebc75e23b0f90523e6afa55044d712fccd0a
nickpwells <wells.nick53@gmail.com>
2015-06-22 19:09:37 -0700

    added form for data entry

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
 	$.ajax({
+		url: 'https://profootballapi.com/players',
   		type: 'POST',
-  		url: 'https://profootballapi.com/players',
   		data: {"api_key": "RVmnd4sEiJY7PeA0MvQ5UquZW1poBGa3",
   				"stats_type": "offense"},
   		dataType: 'jsonp',
