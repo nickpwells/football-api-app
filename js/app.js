62737f1037ff2f17eda3b7b117eaf9c2a28156a3
nickpwells <wells.nick53@gmail.com>
2015-06-27 08:13:30 -0700

    created function to populate stats

--- js/app.js
+++ js/app.js
@@ -1,36 +1,32 @@
 $(document).ready(function(){
 
-	/*$.ajax({
-		type: 'POST',
-		url: "https://profootballapi.com/players",
-		api_key: 'RVmnd4sEiJY7PeA0MvQ5UquZW1poBGa3',
-		stats_type: 'offense',
-		player_name: "p.manning",
-		year: "2014",
-		month: "11",
-		dataType: "json"
-	})
-	.done(function(data){
-		console.log(data);
-	});*/
+	var stats = {
+	}
+	var i = 1;
+
+	//populates stats objects by iterating through JSON data in each week
+	function createStatsObject (attempts, completions, yards, touchdowns, interceptions) {
+		var week = "Week " + i;
+		stats[week] = [attempts, completions, yards, touchdowns, interceptions];
+		i+=1;
+	}
 
 	$.ajax('https://profootballapi.com/players', 
 		{type: 'POST', 
-		data: {"api_key":"RVmnd4sEiJY7PeA0MvQ5UquZW1poBGa3","stats_type":"offense",'year':'2014','player_name':'P.Manning'}, 
+		data: {"api_key":"RVmnd4sEiJY7PeA0MvQ5UquZW1poBGa3",
+		"stats_type":"offense",
+		'year':'2014',
+		"player_name":"P.Manning"},
 		success: function(data, status) {
-        	console.log(JSON.parse(data));
-        	console.log(status);
-    	}
-	});
+			var obj = JSON.parse(data);
+			$.each(obj, function(key, week){
+				$.each(week, function(key, player_id){
+					var passingObj = this.passing;
+					createStatsObject(passingObj.attempts, passingObj.completions, passingObj.yards, passingObj.touchdowns, passingObj.interceptions);
+				});
+			});
+		}
+    });
 
-	function addPlayer() {
-		$(".player-form:last-child").clone()
-									.find("input:text").val("").end()
-									.appendTo(".main-content-container");
-	}
-
-	$(".main-content-container").on("click",".add-player",function(e){
-		e.preventDefault();
-		addPlayer();
-	});
+    console.log(stats);
 });
