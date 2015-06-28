db1a642c737f8ff67c6cecdb5013791305c022b0
nickpwells <wells.nick53@gmail.com>
2015-06-28 12:52:16 -0700

    modified stats object to differentiate between different types

--- js/app.js
+++ js/app.js
@@ -1,32 +1,34 @@
 $(document).ready(function(){
 
-	var stats = {
-	}
-	var i = 1;
-
-	//populates stats objects by iterating through JSON data in each week
-	function createStatsObject (attempts, completions, yards, touchdowns, interceptions) {
-		var week = "Week " + i;
-		stats[week] = [attempts, completions, yards, touchdowns, interceptions];
-		i+=1;
-	}
+	var stats = {};//stats object which will be populated by JSON data
+	var i = 1;//used to count each week of stats
 
 	$.ajax('https://profootballapi.com/players', 
 		{type: 'POST', 
 		data: {"api_key":"RVmnd4sEiJY7PeA0MvQ5UquZW1poBGa3",
 		"stats_type":"offense",
 		'year':'2014',
-		"player_name":"P.Manning"},
+		"player_name":"M.Lynch"},
 		success: function(data, status) {
 			var obj = JSON.parse(data);
 			$.each(obj, function(key, week){
 				$.each(week, function(key, player_id){
 					var passingObj = this.passing;
-					createStatsObject(passingObj.attempts, passingObj.completions, passingObj.yards, passingObj.touchdowns, passingObj.interceptions);
+					var rushingObj = this.rushing;
+					var receivingObj = this.receiving;
+
+					var week = "Week " + i;
+					stats[week] = {};
+
+					//checks if player has referenced stats for the current week.  if yes, will populate stats object with appropriate stats
+					if (player_id.hasOwnProperty('passing')) {stats[week]["passing"] = [passingObj.attempts, passingObj.completions, passingObj.yards, passingObj.touchdowns, passingObj.interceptions];}
+					if (player_id.hasOwnProperty('rushing')) {stats[week]["rushing"] = [rushingObj.attempts, rushingObj.yards, rushingObj.touchdowns, rushingObj.long];}
+					if (player_id.hasOwnProperty('receiving')) {stats[week]["receiving"] = [receivingObj.receptions, receivingObj.yards, receivingObj.touchdowns, receivingObj.long];}
+					i += 1;
 				});
 			});
 		}
-    });
-
-    console.log(stats);
+    	}
+    );
+	console.log(stats);
 });
